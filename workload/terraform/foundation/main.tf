4c4a406036afc5b868126aba278aaf952021cb9e
gxxtrp <128902701+gxxtrp@users.noreply.github.com>
2026-09-10 11:25:02 +0700

    fix: enable Cloud DNS for managed Kafka networking

--- workload/terraform/foundation/main.tf
+++ workload/terraform/foundation/main.tf
@@ -4,6 +4,7 @@ locals {
     "cloudbuild.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "compute.googleapis.com",
+    "dns.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com",
     "monitoring.googleapis.com",
